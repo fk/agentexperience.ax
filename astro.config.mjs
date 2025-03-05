f78a8b88992fb214616544da25b93eb8f85f315f
Sean Roberts <sean.roberts@netlify.com>
2025-03-05 02:02:54 -0500

    adding sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,42 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon"
 import rehypeFigure from '@microflash/rehype-figure'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  site: 'https://agentexperience.ax',
+  integrations: [
+    tailwind(),
+    icon(),
+    sitemap({
+      // because we have a lot of in flight page structures, we are
+      // allow listing what goes into the sitemap.
+      filter: (page) => {
+
+        const {pathname} = new URL(page);
+
+        switch (pathname){
+          case '/':
+          case '/articles/':
+          case '/research/':
+            return true;
+          default:
+            break;
+        }
+
+        if (pathname.startsWith('/research/')) {
+          return true;
+        }
+
+        if (pathname.startsWith('/concepts/')) {
+          return true;
+        }
+
+        return false;
+      }
+    })
+  ],
   markdown: {
     rehypePlugins: [rehypeFigure],
   },
